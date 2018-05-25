7c86575bcab54f7d83f402800d2eac7e9a2ead8c
Rohit Tiwari <rohit.beawar@gmail.com>
2018-05-25 17:44:34 +0530

    ribbon added

---
+++ EasyExcel/EasyExcel/EasyExcelRibbon1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Tools.Ribbon;
+
+namespace EasyExcel
+{
+    public partial class EasyExcelRibbon1
+    {
+        private void EasyExcelRibbon1_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, RibbonControlEventArgs e)
+        {
+            //save the workbook on its current state
+        }
+    }
+}
---
+++ EasyExcel/EasyExcel/TabRibbon1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.Office.Tools.Ribbon;
+
+namespace EasyExcel
+{
+    public partial class TabRibbon1
+    {
+        private void TabRibbon1_Load(object sender, RibbonUIEventArgs e)
+        {
+           // MessageBox.Show("Load");
+        }
+
+       
+
+        private void WorkbookSnap_click(object sender, RibbonControlEventArgs e)
+        {
+            
+        }
+    }
+}
